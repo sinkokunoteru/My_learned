9b461d2f23a3f7053494060873a744c53eb7dcfc
sinkokunoteru <40794418+sinkokunoteru@users.noreply.github.com>
2020-06-15 17:08:42 +0900

    Add files via upload

--- Til/cook_resolution/recipe_data_collect.py
+++ Til/cook_resolution/recipe_data_collect.py
@@ -3,25 +3,26 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 class html_read():
-    text=[]
-    lines=[]
+    def __init__(self):
+        self.text=[]
+        self.lines=[]
 
-    def require_html():
+    def require_html(self):
         html=requests.get("https://chefgohan.gnavi.co.jp/detail/90").text
         return html
 
-    def create_html_list(html):
+    def create_html_list(self,html):
         soup=BeautifulSoup(html,"html.parser")  #htmlをすべて取得
         for script in soup(["script,style"]):   #scriptやstyleを含む要素を削除
             script.decompose()
-        text.self=soup.get_text()    #テキストのみを取る
-        lines.self=[line.strip() for line in text.splitlines()]  #textを改行ごとにリストに入れる
-        text.self="\n".join(line for line in lines.self if line)  #リストの空白要素以外をすべて文字列に戻す
+        self.text=soup.get_text()    #テキストのみを取る
+        self.lines=[line.strip() for line in self.text.splitlines()]  #textを改行ごとにリストに入れる
+        self.text="\n".join(line for line in self.lines if line)  #リストの空白要素以外をすべて文字列に戻す
 
-    def html_material(text):
+    def html_material(self,text):
         start=int(text.find("■材料"))
         end=int(text.find("■この料理の作り方"))
-        material=text.self[start:end]
+        material=self.text[start:end]
         return material
 
 #def SQL_save():
@@ -29,9 +30,9 @@ class html_read():
 
 def main():
     h=html_read()
-    html=require_html()
-    create_html_list(html)
-    material=html_material(text)
+    html=h.require_html()
+    h.create_html_list(html)
+    material=h.html_material(h.text)
     print(material)
     #SQLにデータを保存
     #
